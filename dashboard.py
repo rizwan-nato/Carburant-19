d133fac2def83c5cf39e7f7731509960b7cdb7ce
Rizwan Nato <rizwanato@hotmail.fr>
2022-03-13 17:17:59 +0100

    try geolocation

--- dashboard.py
+++ dashboard.py
@@ -15,14 +15,46 @@ from time import time
 import streamlit_analytics.streamlit_analytics as streamlit_analytics
 import pickle
 import altair as alt
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
 with streamlit_analytics.track():
 
 
     update_data_instant()
     update_data_anual()
+    loc_button = Button(label="Get Device Location", max_width=150)
+    loc_button.js_on_event(
+        "button_click",
+        CustomJS(
+            code="""
+        navigator.geolocation.getCurrentPosition(
+            (loc) => {
+                document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+            }
+        )
+        """
+        ),
+    )
+    result = streamlit_bokeh_events(
+        loc_button,
+        events="GET_LOCATION",
+        key="get_location",
+        refresh_on_update=False,
+        override_height=75,
+        debounce_time=0,
+    )
+
+    if result:
+        if "GET_LOCATION" in result:
+            loc = result.get("GET_LOCATION")
+            lat = loc.get("lat")
+            lon = loc.get("lon")
+            st.write(f"Lat, Lon: {lat}, {lon}")
 
     st.title("Comparateur de station")
+  
     st.write("Où sont les stations essence les moins chères autour de vous ? Voici une carte mise à jour quotidiennement. Choisissez le type de carburant recherché, l'adresse ainsi la distance de recherche dans les filtres.")
     BASE_CWD = os.getcwd()
     PATH_DATA = BASE_CWD + "/data"
